a34eff6d0251a776307f2ca2582ea5dc53709210
Rene Garcia <garciamx@gmail.com>
2013-01-31 12:21:43 -0600

    it works on the wild wild web!

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
+
 import (
-    "net/http"
-    "io"
-    "log"
-    "os"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 func HelloServer(w http.ResponseWriter, req *http.Request) {
-    io.WriteString(w, "HELLO WORLD")
+	io.WriteString(w, "HELLO WORLD")
 }
 
 func main() {
-    http.HandleFunc("/", HelloServer)
-    http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
-    http.Handle("/data/olympiads/", http.StripPrefix("/data/olympiads/", http.FileServer(http.Dir("data/olympiads"))))
-    http.HandleFunc("/olympiad", olympiadHandler)
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    if err != nil {
-        log.Fatal("ListenAndServe", err)
-    }
+	http.HandleFunc("/", HelloServer)
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	http.Handle("/data/olympiads/", http.StripPrefix("/data/olympiads/", http.FileServer(http.Dir("data/olympiads"))))
+	http.HandleFunc("/olympiad", olympiadHandler)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		log.Fatal("ListenAndServe", err)
+	}
 }
-
--- physicsOlympiad.go
+++ physicsOlympiad.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"labix.org/v2/mgo"
-	"strconv"
 	"html/template"
+	"labix.org/v2/mgo"
+	"net/http"
+	"strconv"
+
 //   "labix.org/v2/mgo/bson"	
 )
 
-const DB_LOCATION = "localhost:27017"
-const DATABASE = "physicsolympiad"
+const (
+	DB_LOCAL = "localhost"
+	DB_HOST  = "mongodb://timeChamber:123456@linus.mongohq.com:10086/physicsolympiad"
+	DATABASE = "physicsolympiad"
+)
 
-type dict map[string] interface{}
+type dict map[string]interface{}
 
 type Olympiad struct {
-	Year int64  `year`
-	City string  `city`
+	Year    int64  `year`
+	City    string `city`
 	Country string `country`
 }
 
@@ -35,27 +39,27 @@ func olympiadHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	session, err := mgo.Dial(DB_LOCATION)
+	session, err := mgo.Dial(DB_HOST)
 	if err != nil {
 		fmt.Fprintln(w, "Dial:", err)
-        return
+		return
 	}
 
 	defer session.Close()
-	
+
 	c := session.DB(DATABASE).C(collection)
-	
+
 	var olympiads []Olympiad
 	err = c.Find(nil).Limit(100).All(&olympiads)
 	//err = c.Find(bson.M{"year":1998}).Limit(100).All(result)
-	
+
 	if err != nil {
 		fmt.Fprintln(w, "Find:", err)
 		return
 	}
-	
+
 	var this Olympiad
-	
+
 	for _, olympiad := range olympiads {
 		if olympiad.Year == document {
 			this = olympiad
@@ -63,11 +67,10 @@ func olympiadHandler(w http.ResponseWriter, req *http.Request) {
 	}
 
 	tmpl := template.Must(template.ParseFiles("templates/olympiad.html", "templates/navbar.html"))
-	err = tmpl.Execute(w, dict {"Collection": collection ,"Olympiads": olympiads, "Document": this})
-	
+	err = tmpl.Execute(w, dict{"Collection": collection, "Olympiads": olympiads, "Document": this})
+
 	if err != nil {
 		fmt.Fprintln(w, "Execute:", err)
 		return
 	}
 }
-
